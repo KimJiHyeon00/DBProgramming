87b887ace5f0f557aef3716901588a5819d38c70
김지현 <jihyeon1542@gmail.com>
2020-10-13 20:46:56 +0900

    add_MarketBrowser.sin

---
+++ MarketBrowser/MarketBrowser/MainForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace MarketBrowser
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOpenCSV = new System.Windows.Forms.Button();
+            this.textBoxCSVViewer = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonOpenCSV
+            // 
+            this.buttonOpenCSV.Location = new System.Drawing.Point(31, 35);
+            this.buttonOpenCSV.Name = "buttonOpenCSV";
+            this.buttonOpenCSV.Size = new System.Drawing.Size(170, 46);
+            this.buttonOpenCSV.TabIndex = 1;
+            this.buttonOpenCSV.Text = "CSV열기";
+            this.buttonOpenCSV.UseVisualStyleBackColor = true;
+            this.buttonOpenCSV.Click += new System.EventHandler(this.buttonOpenCSV_Click);
+            // 
+            // textBoxCSVViewer
+            // 
+            this.textBoxCSVViewer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxCSVViewer.Location = new System.Drawing.Point(12, 98);
+            this.textBoxCSVViewer.Multiline = true;
+            this.textBoxCSVViewer.Name = "textBoxCSVViewer";
+            this.textBoxCSVViewer.Size = new System.Drawing.Size(757, 340);
+            this.textBoxCSVViewer.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxCSVViewer);
+            this.Controls.Add(this.buttonOpenCSV);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOpenCSV;
+        private System.Windows.Forms.TextBox textBoxCSVViewer;
+    }
+}
+
---
+++ MarketBrowser/MarketBrowser/MainForm.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Markup;
+
+namespace MarketBrowser
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            int a = 0; //일부러 적음
+        }
+
+        private List<List<string>> MarketColumnarDataStructure()
+        {
+            StreamReader sr = new StreamReader(new FileStream("market.csv", FileMode.Open), Encoding.Default); //지금은 상태경로 | c:\\ ->절대경로
+
+            String str = "";
+
+            var line = sr.ReadLine();
+            var headers = line.Split(',');
+
+
+            List<List<string>> data = new List<List<string>>(); //<<>>리스트에 리스트를 담는 것
+            foreach (String header in headers)
+            {
+                List<string> list = new List<string>(); // 빈 리스트에 칼럼들을 채워넣겠다는 뜻
+                list.Add(header); //리스트라는 컬렉션에 header 텍스트가 젤 첫번재로 들어감
+                data.Add(list);
+            }
+
+            //textBoxCSVViewer.Text += header + "\t";
+
+            int row = 0;          
+            while (sr.EndOfStream == false)
+            {
+                line = sr.ReadLine(); //하나의 라인이 들어감
+                var values = line.Split(','); //콤마 기준으로 단어들 뽑아줌
+                // |우리시장| |5일장| |대구시|...
+                
+                for (int i=0; i<values.Length; i++)
+                {
+                    data[i].Add(values[i]);
+                }
+                if (row++ == 10)
+                    break;
+                //str += values[0] + "\r\n"; //values의 0번째 컬럼만 출력해줌
+                //textBoxCSVViewer.Text += line + "\r\n"; //파일이 라인 단위로 출력됌
+            }
+
+            //textBoxCSVViewer.Text = str;
+
+            sr.Close();
+            return data;
+        }
+
+        private void printColumnData_columnbased(List<List<string>> data, int columnIdx)
+        {
+            string str = ""; //두번째 컬럼 나차내기
+            textBoxCSVViewer.Text = "";
+            foreach (string value in data[columnIdx])
+            {
+                str += value + "\r\n"; //빠르게 업데이트
+            }
+            textBoxCSVViewer.Text = str;//텍스트박스 걔속 업데이트하게 함
+        }
+
+        private void printRowData_columnbased(List<List<string>> data)
+        {
+            string str = "";
+            for (int row = 0 ; row < data[0].Count; row++) //첫번째 리스트 길이만큼 출력
+            { 
+                foreach(List<string> list in data)
+                {
+                    str += list[row] + "\t";
+                }
+                str += "\r\n";
+            }
+            textBoxCSVViewer.Text = str;
+        }
+        private string[] parseCSVLine(string line)
+        {
+            return line.Split(',');
+        }
+        private List<List<string>> MakeRowbasedDataStructure()
+        {
+            StreamReader sr = new StreamReader(new FileStream("market.csv", FileMode.Open), Encoding.Default); //지금은 상태경로 | c:\\ ->절대경로
+
+            var line = sr.ReadLine();
+           
+            List<List<string>> data = new List<List<string>>(); //<<>>리스트에 리스트를 담는 것
+        
+            while (sr.EndOfStream == false)
+            {
+                line = sr.ReadLine(); //하나의 라인이 들어감
+                var values = parseCSVLine(line); //콤마 기준으로 단어들 뽑아줌
+                // "시장(구, 우리시장)" -> 콤마 없애는 코드 과제!!                            // |우리시장| |5일장| |대구시|...
+                // 따옴표가 나오면 다음번의 따옴표가 나올때까지 하나의 문자로 묶어주면 끝 / 하나의 string으로 만들어줘라! 
+                data.Add(values.ToList());
+                 /*
+                if (row++ == 10) //현재로써는 이 코드 없으면 구동안됌
+                    break;  
+                 */
+
+                //지금 커밋해봐도되는거죠? 네네!
+            }
+            sr.Close();
+            return data;
+        }
+
+        private void printRowData_rowbased(List<List<string>> data)
+        {
+            string str = "";
+            foreach (List<string> rowlist in data)
+            {
+                foreach (string value in rowlist)
+                    str += value + "\t";
+                str += "\r\n";
+            }
+            textBoxCSVViewer.Text = str;
+        }
+    
+        private void printColumnData_rowbased(List<List<string>> data, int colIdx)
+        {
+            string str = "";
+            foreach (List<string> rowlist in data)
+            {
+                str += rowlist[colIdx] + "\r\n";
+            }
+            textBoxCSVViewer.Text = str;
+        }
+
+        private void buttonOpenCSV_Click(object sender, EventArgs e)
+        {
+            List<List<string>> data = MakeRowbasedDataStructure();
+            //printRowData_rowbased(data);
+            printColumnData_rowbased(data, 1);
+            
+
+            //List<List<string>> data=  MarketColumnarDataStructure();
+            //printColumnData_columnbased(data, 1);
+            //printRowData_columnbased(data);
+
+        }
+    }
+}
